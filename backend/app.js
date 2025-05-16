3ca6a6bdef520b542077c1c40514c294647f6e55
LegendXAnurag <anurag.prasad955@gmail.com>
2025-05-16 11:58:03 +0530

    Added basic backend code and connection to mongo db database

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {default:mongoose} = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const DB_PATH = process.env.DB_URI
+const PORT = process.env.PORT
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+
+mongoose.connect(DB_PATH).then(()=>{
+    console.log("Connected to MongoDB via mongoose")
+    app.listen(PORT,()=>{
+        console.log(`server running on address http://localhost:${PORT}`)
+    })
+}).catch(err=>{
+    console.log("Error Connecting to MongoDB", err);
+})
